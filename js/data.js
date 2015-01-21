b8bfbd029e418e376d90bc1707d02bb9694040c1
Yuval Bennaim <yuvalbennaim@gmail.com>
2015-01-21 14:08:31 -0700

    add

--- js/data.js
+++ js/data.js
@@ -93,8 +93,7 @@ window.jobData = [
       "date_to": "Current",
       "position": "Sr Software Developer",
       "logo": "images/itriage.jpg",
-      "details": "At SErena I built...",
-      "$$hashKey": "008"
+      "detail": "Angular.JS development"
   },
   {
       "company": "Oppenheimer Gunds Inc.",
@@ -102,8 +101,6 @@ window.jobData = [
       "date_to": "2014-12-31",
       "position": "Sr Software Developer, UI Architect",
       "logo": "images/ofi.png",
-      "details": "At SErena I built...",
-      "$$hashKey": "009",
       "detail": "Web and Mobile Development using HTML5, JavaScript, Angular.JS, JQuery, CSS3, PhoneGap, MongoDB, Node.js and Ruby on Rails.\n\nArchitected and led the development effort of a major product which allows financial Advisers to create and manage their clients investment portfolios."
   },
   {
@@ -112,8 +109,6 @@ window.jobData = [
       "date_to": "2013-06-01",
       "position": "Sr Software Developer",
       "logo": "images/pearson.jpg",
-      "details": "At SErena I built...",
-      "$$hashKey": "00A",
       "detail": "Developing and porting of a Web App from Dojo to jQuery and AngularJS."
   },
   {
@@ -122,8 +117,6 @@ window.jobData = [
       "date_to": "2013-01-01",
       "position": "Sr Software Developer",
       "logo": "images/clickfox.png",
-      "details": "At SErena I built...",
-      "$$hashKey": "00B",
       "detail": "Developed advanced big data visualizations for Desktop, Web and Mobile."
   },
   {
@@ -132,8 +125,6 @@ window.jobData = [
       "date_to": "2012-06-15",
       "position": "Sr Software Developer",
       "logo": "images/serena.jpg",
-      "details": "At SErena I built...",
-      "$$hashKey": "00C",
       "detail": "Developed the User Interfaces for the “Serena Request Center”, both a jQuery based Web Client, as well as an innovative Mobile Application for iOS and Android. devices.\n\nBefore that, I have been the Lead Developer on the Web User Interfaces of the company’s flagship product “Serena Business Manager”, AKA “TeamTrack”. \n\nThe thin Web Client is cross browser (IE, FireFox, Mozilla, Safari), Section 508 Compliant, Internationalized, and utilizes AJAX for Rich User Interaction and performance. The server is a proprietary C++ application where 50% of the UI generation takes place, and the other half is dynamically created on the client using JavaScript and DOM manipulations.\n\nBefore joining the TeamTrack team, I was the UI Lead Developer for the company’s Distributed Software Change Management system, ChangeMan DS.  This was a Multi-tiered, MVC Client-Server application. The Client was built using Java Swing.\n\nIn my earlier years at Serena, I created various tools and utilities, including a  specialized JDBC Driver  that translated on-the-fly SQL queries written initially for SQLSERVER to Oracle and DB2 SQL syntax, a Java Swing WYSIWYG IDE for creating custom forms for the company’s early ALM tool, a Swing Document Diff / Merge Application, and an SWT Eclipse Plug-in."
   },
   {
@@ -142,8 +133,6 @@ window.jobData = [
       "date_to": "2001-04-10",
       "position": "Java Architect",
       "logo": "images/carsdirect.png",
-      "details": "At SErena I built...",
-      "$$hashKey": "00D",
       "detail": "Joined this startup as a Senior Java Developer. Facilitated the migration of the set from IIS / SQLSERVER / ASP to a complete J2EE multi-tiered architecture running on clustered Oracle and Weblogic Application servers. \n\nBuilt the back office messaging (MQ Series and JMS) Enterprise Application Integration (EAI) between the CRM system (Silknet) and financial (Lawson) systems. \n\nDeveloped a Network\\Server Monitoring and Recovery Application in Java. This application was a live display of the production servers, monitoring their performance and  health statistics, and remote administration."
   },
   {
@@ -152,8 +141,6 @@ window.jobData = [
       "date_to": "1999-02-21",
       "position": "Director of R&D",
       "logo": "images/modacad.jpg",
-      "details": "At SErena I built...",
-      "$$hashKey": "00E",
       "detail": "Joined this company 2 years after its IPO. ModaCAD was a rising star in the  area of CAD applications geared for the Apparel and Furniture industries. We had a few boxed, consumer products like 3D Home Interior which was sold under an OEM to Broderbund Software.\n\nFor my fist project at ModaCAD, I created a CAD application called ModaWeave which simulated the design of Woven fabrics. This was a professional design software utilized d by fashion designers at companies like Lands End, the Gap, Levis, Janzen etc. to prototype woven fabrics, and using our other applications, create realistic 3D renderings.\n \nThis application was developed with Java 1.1, with it’s very primitives Graphics AWT library. It was natively compiled for Windows using an IDE called SuperCede. To the best of my knowledge, this may have been the first ever CAD application that was written exclusively in Java. \n\nNext at ModaCAD, I was the technical lead of a project, and web site called StyleClick.com. This was a fashion portal and E-Commerce site that used Java Servlets, JDBC and Weblogic Application Server.\n\nOne of the final projects I worked on at ModaCAD was a Fuzzy Logic based image comparison engine used for Fabric sample Matching. The same engine was later used to build a Fashion Recommendation Engine, and was one of the reasons \n\nIn 1998, riding the .com wave, ModaCAD renamed itself as StyleClick.com, and sold off its core CAD business. At the time I was tasked with creating a data syndication engine, and with XML becoming popular at the time, designed a set of XML over HTTP Web services. This was before the term was officially coined, and the SOAP protocol was conceived.\n\nIn 1999 ModaCAD was bought by USA Networks, and has since dissolved."
   },
   {
@@ -162,8 +149,6 @@ window.jobData = [
       "date_to": "1997-08-08",
       "position": "Owner, Animator",
       "logo": "images/qubix.png",
-      "details": "At SErena I built...",
-      "$$hashKey": "00F",
       "detail": "Along with a partner, established this Multi Media, 3D Animation studio. \nLater, with the explosion of the Web, migrated more towards web development.\nWhile my partner handled most of the Server and Database aspects of the various projects, I was primarily focused on the User Interfaces. Designed numerous Web sites using HTML, Perl scripts, Java Scripts, Java Applets, ASP etc.\nOur biggest client were:\nXerox – 3d animations for corporate sale videos, and technical presentations.\nLAX – designed a large scale database application for the Airport’s Sound Retrofitting Project.\nJdate.com -  Jdate was the first dating service on the web geared towards the Jewish community."
   }
 ]
